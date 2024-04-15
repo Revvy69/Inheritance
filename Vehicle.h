e431378bfb0400d1cea2fd77d2d548916f57a30d
Revvy69 <94577296+Revvy69@users.noreply.github.com>
2024-04-15 16:05:15 -0400

    Update Vehicle.h
    Added code that will be used in Vehicle.cpp

--- Vehicle.h
+++ Vehicle.h
@@ -1,1 +1,22 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
 
+#include <string>
+using namespace std;
+
+class Vehicle {
+protected:
+  string manufacturer;
+  int yearBuilt;
+
+public: 
+Vehicle(const string manufacturer, int yearBuilt);
+string getManufacturer() const;
+void setManufacturer(const string manufacturer);
+int getYearBuilt() const;
+void setYearBuilt(int yearBuilt);
+void displayInfo() const;
+}
+#endif // VEHICLE_H
+
+// this file sets up variables, functions, and the class for Vehicles which will be used in Vehicle.cpp 
